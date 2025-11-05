6a51ba98b3fc5c08669d33ca7bf66cd6a22c5a5a
sathushan64 <sathushang4756@gmail.com>
2025-11-05 13:21:47 +0530

    1st commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
-/** @type import('hardhat/config').HardhatUserConfig */ 
 require("@nomicfoundation/hardhat-toolbox"); 
+
+
 module.exports = { 
   solidity: "0.8.19", 
   networks: { 
---
+++ pages/voterList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const VoterList = () => {
+  return (
+    <div className="container">
+      <h1>Voter List</h1>
+      <div className="voter-list">
+        <p>Voter list functionality will be implemented here</p>
+        {/* Add your voter list content here */}
+      </div>
+    </div>
+  );
+};
+
+export default VoterList;
